9350ca88720b4f25f8bd5bd37411bbb389f033c7
jarbasai <jarbasai@mailfence.com>
2022-02-05 17:08:26 +0000

    skill settings initialize + ocp

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-import random
 from os.path import join, dirname
 
 from ovos_plugin_common_play.ocp import MediaType, PlaybackType
+from ovos_utils.log import LOG
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
     ocp_search, ocp_featured_media
-from youtube_archivist import YoutubeArchivist
+from youtube_archivist import YoutubeMonitor
 
 
 class SovietWaveSkill(OVOSCommonPlaybackSkill):
@@ -16,21 +16,15 @@ class SovietWaveSkill(OVOSCommonPlaybackSkill):
         self.supported_media = [MediaType.GENERIC,
                                 MediaType.RADIO,
                                 MediaType.MUSIC]
-        self.archive = YoutubeArchivist(db_name="SovietWave")
+        self.archive = YoutubeMonitor(db_name="SovietWave", logger=LOG)
 
     def initialize(self):
-        if len(self.archive.db) == 0:
-            # no database, sync right away
-            self.schedule_event(self._scheduled_update, 5)
-
-    def _scheduled_update(self):
-        self.update_db()
-        self.schedule_event(self._scheduled_update, random.randint(3600, 12 * 3600))  # every 6 hours
-
-    def update_db(self):
         url = "https://www.youtube.com/c/NewSovietWave"
-        self.archive.archive(url)
-        self.archive.remove_unavailable()  # check if video is still available
+        bootstrap = f"https://raw.githubusercontent.com/OpenJarbas/streamindex/main/{self.archive.db.name}.json"
+        self.archive.bootstrap_from_url(bootstrap)
+        self.archive.monitor(url)
+        self.archive.setDaemon(True)
+        self.archive.start()
 
     @ocp_search()
     def ocp_sovietwave_radio(self, phrase, media_type):
@@ -40,17 +34,17 @@ class SovietWaveSkill(OVOSCommonPlaybackSkill):
     @ocp_featured_media()
     def featured_media(self):
         return [
-                {
-                    "match_confidence": 100,
-                    "media_type": MediaType.MUSIC,
-                    "uri": "youtube//" + entry["url"],
-                    "playback": PlaybackType.AUDIO,
-                    "image": entry["thumbnail"],
-                    "length": entry.get("duration", 0) * 1000,
-                    "bg_image": self.default_bg,
-                    "skill_icon": self.skill_icon,
-                    "title": entry["title"]
-                } for entry in self.archive.sorted_entries()
+            {
+                "match_confidence": 100,
+                "media_type": MediaType.MUSIC,
+                "uri": "youtube//" + entry["url"],
+                "playback": PlaybackType.AUDIO,
+                "image": entry["thumbnail"],
+                "length": entry.get("duration", 0) * 1000,
+                "bg_image": self.default_bg,
+                "skill_icon": self.skill_icon,
+                "title": entry["title"]
+            } for entry in self.archive.sorted_entries()
         ]
 
 
